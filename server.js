b65d04af582110b57b0d6fb5d41e6d3753a7a637
andif_injani@bitbucket.com <andif@injani.com>
2019-03-05 13:40:49 +0700

    ok

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var events = require('events');
 var spawn = child.spawn;
 
 
-var url ='https://keeplive.online/streamIn/';
+var url ='http://localhost:3000/streamIn/';
 var uid = "andy";
 
 console.log('Starting FFMPEG')
 var ffmpegString = '-i '+url+''
 ffmpegString += ' -f mpegts -c:v mpeg1video -an '+url+uid;
-ffmpegString += ' -f mpegts -c:v mpeg1video -an '+url+uid+'2';
 
 if(ffmpegString.indexOf('rtsp://')>-1){
     ffmpegString='-rtsp_transport tcp '+ffmpegString
