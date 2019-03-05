f1f2830b9162082ca944eafa0c7acd9b4521ef58
andif_injani@bitbucket.com <andif@injani.com>
2019-03-05 12:30:38 +0700

    send

---
+++ server.js
@@ -0,0 +1,23 @@
+var child = require('child_process');
+var events = require('events');
+var spawn = child.spawn;
+
+
+var url ='https://keeplive.online/play/';
+var uid = "andy";
+
+console.log('Starting FFMPEG')
+var ffmpegString = '-i '+config.url+''
+ffmpegString += ' -f mpegts -c:v mpeg1video -an '+url+uid;
+ffmpegString += ' -f mpegts -c:v mpeg1video -an '+url+uid+'2';
+
+if(ffmpegString.indexOf('rtsp://')>-1){
+    ffmpegString='-rtsp_transport tcp '+ffmpegString
+}
+console.log('Executing : ffmpeg '+ffmpegString)
+var ffmpeg = spawn('ffmpeg',ffmpegString.split(' '));
+ffmpeg.on('close', function (buffer) {
+    console.log('ffmpeg died')
+})
+
+console.log("run at "+url+uid)
