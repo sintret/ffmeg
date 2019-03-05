1ab9033c1f9dd08ff9a3e195160354529a75d441
andif_injani@bitbucket.com <andif@injani.com>
2019-03-06 02:14:58 +0700

    ok

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ var unique = {
 
 var command = "sudo modprobe bcm2835-v4l2";
 cmd.run(command);
+console.log(command)
 
 var command2 = "ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://68.183.236.92:" +unique.anggrek10.url;
 cmd.run(command2);
+console.log(command2)
